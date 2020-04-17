21654ae2e146735b1859dd2b01a728d6e82d867c
christoschr97 <christoschristodoulou3@gmail.com>
2020-04-17 15:42:44 +0300

    add

---
+++ src/sample/ConnectToDB.java
@@ -0,0 +1,26 @@
+package sample;
+import java.sql.*;
+public class ConnectToDB {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String user = "root";
+        String password = "";
+
+        try {
+            Connection myConn = DriverManager.getConnection(url, user, password);
+            String sql = "select * from mydb.contacts";
+            Statement myStmt = myConn.createStatement();
+            ResultSet rs = myStmt.executeQuery(sql);
+            while(rs.next()) {
+                System.out.println("first: " + rs.getInt(1));
+                System.out.println("second: " + rs.getString(2));
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
